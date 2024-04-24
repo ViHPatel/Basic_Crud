21fd45dcd29f10ddecc27cdd0b5b9ab0f85ba81f
Vidhi Patel <vidhih1605@gmail.com>
2024-04-24 16:55:38 +0530

    "Initial commit"

---
+++ Data/BasicCRUDContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BasicCRUD.Model;
+
+namespace BasicCRUD.Data
+{
+    public class BasicCRUDContext : DbContext
+    {
+        public BasicCRUDContext (DbContextOptions<BasicCRUDContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BasicCRUD.Model.User> User { get; set; } = default!;
+    }
+}
---
+++ Pages/Users/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using BasicCRUD.Data;
+using BasicCRUD.Model;
+
+namespace BasicCRUD.Pages.Users
+{
+    public class IndexModel : PageModel
+    {
+        private readonly BasicCRUD.Data.BasicCRUDContext _context;
+
+        public IndexModel(BasicCRUD.Data.BasicCRUDContext context)
+        {
+            _context = context;
+        }
+
+        public IList<User> User { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            User = await _context.User.ToListAsync();
+        }
+    }
+}
